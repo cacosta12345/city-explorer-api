323983245634099826e2aec77b156fed33ad98d8
Christopher Acosta <chrisacosta12345@gmail.com>
2023-12-06 15:48:35 -0800

    trying to get weather api to work

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 'use strict';
 
-let weather = require('./data/weather.json');
 
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios")
 
 const app = express();
 
@@ -12,24 +12,37 @@ app.use( cors() );
 
 const PORT = process.env.PORT || 3000;
 
+// class Forecast {
+//     constructor(weatherData){
+//         this.date= weatherData.data.valid_date;
+//         this.description = weatherData.data.weather.description;
+//     }
+// }
+
 app.get('/', (request, response) => {
-    let data = { message: "Goodbye World"};
+    let data = { message: "Hello World"};
     response.json(data);
 });
 
-app.get('/weather', (request, response)=>{
+app.get('/weather', getWeatherData);
+
+async function getWeatherData(request,response){
     let lat = request.query.lat
     let lon = request.query.lon
-    let city = request.query.searchQuery
-    const cityInfo = weather.find((cityData)=>{
-       return cityData.city_name === city;
-    })
-    if(cityInfo){
-       return response.json({cityInfo})
-    }
-    response.status(404).json({error:'you done goofed'}) 
-   
-})
+    
+    let axiosResponse = await axios.get('https://api.weatherbit.io/v2.0/forecast/daily?',
+    {
+        params: {
+            lat: lat,
+            lon: lon,
+            key: process.env.WEATHER_API_KEY
+        }
+    });
+    
+    response.json(axiosResponse);
+}
+
+app.get('/')
 
 app.get("*", (request, response) => {
     response.status(404).send("Page Not Avaiable");
